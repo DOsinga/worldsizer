0dbe6253f40aa403683de0805b258767bfca08a5
Douwe Osinga <douwe.osinga@gmail.com>
2020-12-26 19:06:38 -0400

    Add covid

--- country_combination.py
+++ country_combination.py
@@ -1,62 +1,95 @@
 import json
 
+import pycountry
 
-PROPERTIES = {'Area', 'Population', 'GDP (official exchange rate)', 'HIV/AIDS - people living with HIV/AIDS',
-              'Crude oil - proved reserves', 'Exports', 'Land use', 'Labor force'}
+PROPERTIES = {
+    'Area',
+    'Population',
+    'GDP (official exchange rate)',
+    'HIV/AIDS - people living with HIV/AIDS',
+    'Crude oil - proved reserves',
+    'Exports',
+    'Land use',
+    'Labor force',
+}
 
 
 def main():
-  with open('data/cia_data.js') as fin:
-    cia_data = json.load(fin)
+    with open('data/cia_data.js') as fin:
+        cia_data = json.load(fin)
 
-  country_name_to_code = {'united states': 'us',
-                          'soviet union': 'ru',
-                          'great britain': 'gb',
-                          'east germany': 'de',
-                          'czechoslovakia': 'cz',
-                          'yugoslavia': 'rs',
-                          'north korea': 'kp',
-                          'south korea': 'kr',
-                          'iran': 'ir',
-                          'serbia and montenegro': 'rs',
-                          'syria': 'sy',
-                          'vietnam': 'vn',
-                          'venezuela': 've',
-                          'taiwan': 'tw',
-                          'tanzania': 'tz',
-                          'macedonia': 'mk',
-                          'virgin islands': 'vi',
-                          'kosovo': 'xk'}
+    country_name_to_code = {
+        'united states': 'us',
+        'soviet union': 'ru',
+        'great britain': 'gb',
+        'east germany': 'de',
+        'czechoslovakia': 'cz',
+        'yugoslavia': 'rs',
+        'north korea': 'kp',
+        'south korea': 'kr',
+        'iran': 'ir',
+        'serbia and montenegro': 'rs',
+        'syria': 'sy',
+        'vietnam': 'vn',
+        'venezuela': 've',
+        'taiwan': 'tw',
+        'tanzania': 'tz',
+        'macedonia': 'mk',
+        'virgin islands': 'vi',
+        'kosovo': 'xk',
+    }
 
-  data = {cc: {kk: vv for kk, vv in rec.items() if kk in PROPERTIES} for cc, rec in cia_data.items()}
-  with open('data/country_info.json') as fin:
-    for cc in json.load(fin):
-      key = cc['alpha-2'].lower()
-      if key in data:
-        data[key]['name'] = cc['name']
-        country_name_to_code[cc['name'].lower()] = key
-      else:
-        print(key)
+    def ensure_float(v):
+        try:
+            return float(v)
+        except ValueError:
+            return 0
 
-  for key in data:
-    data[key]['medals'] = 0.1
+    data = {
+        cc: {kk: ensure_float(vv) for kk, vv in rec.items() if kk in PROPERTIES}
+        for cc, rec in cia_data.items()
+    }
+    with open('data/country_info.json') as fin:
+        for cc in json.load(fin):
+            key = cc['alpha-2'].lower()
+            if key in data:
+                data[key]['name'] = cc['name']
+                country_name_to_code[cc['name'].lower()] = key
+            else:
+                print(key)
 
-  with open('data/medals.txt', encoding='utf8') as fin:
-    for line in fin:
-      _, country_name, count = line.strip().split('\t')
-      key = country_name_to_code.get(country_name.lower())
-      if key:
-        data[key]['medals'] += int(count)
-      else:
-        print(country_name)
+    for key in data:
+        data[key]['medals'] = 0.1
 
-  with open('data/country_data.js', 'w') as fout:
-    json.dump(data, fout, indent=2)
+    with open('data/medals.txt', encoding='utf8') as fin:
+        for line in fin:
+            _, country_name, count = line.strip().split('\t')
+            key = country_name_to_code.get(country_name.lower())
+            if key:
+                data[key]['medals'] += int(count)
+            else:
+                print(country_name)
+
+    with open('data/owid-covid-latest.json') as fin:
+        IGNORE = {'nan', 'OWID_KOS', 'OWID_WRL'}
+        covid = json.load(fin)
+        three_to_two = {c.alpha_3: c.alpha_2 for c in pycountry.countries}
+        covid = {three_to_two[k]: v for k, v in covid.items() if k not in IGNORE}
+
+    for cc in data:
+        cc_upper = cc.upper()
+        if cc_upper not in covid:
+            print('**', cc_upper)
+            data[cc]['covid_cases'] = 1
+            data[cc]['covid_deaths'] = 1
+        else:
+            data[cc]['covid_cases'] = covid[cc_upper]['total_cases']
+            data[cc]['covid_deaths'] = covid[cc_upper]['total_deaths']
+
+    with open('data/country_data.js', 'w') as fout:
+        fout.write('COUNTRY_DATA = ' + json.dumps(data, indent=2))
+
+
 
 if __name__ == '__main__':
-  main()
-
-
-
-
-
+    main()
--- stateparser.py
+++ stateparser.py
@@ -8,185 +8,238 @@ import json
 from collections import defaultdict, Counter
 
 CC_CODES = {
-  'Taiwan': 'tw',
-  'Western Sahara': 'eh',
+    'Taiwan': 'tw',
+    'Western Sahara': 'eh',
 }
 
 EXTRA_BRIDGES = []
 
+
 def calc_area(pts):
-  n = len(pts) # of corners
-  area = 0.0
-  for i in range(n):
-      j = (i + 1) % n
-      area += pts[i][0] * pts[j][1]
-      area -= pts[j][0] * pts[i][1]
-  return abs(area) / 2.0
+    n = len(pts)  # of corners
+    area = 0.0
+    for i in range(n):
+        j = (i + 1) % n
+        area += pts[i][0] * pts[j][1]
+        area -= pts[j][0] * pts[i][1]
+    return abs(area) / 2.0
 
 
 MAGIC_MOLL_NUMBER = 9020047.848073646
 SRC_PROJ = pyproj.Proj(proj='longlat', ellps='WGS84', datum='WGS84')
-DST_PROJ = pyproj.Proj(proj='moll', lon_0=-70, x_0=0, y_0=0, ellps='WGS84', datum='WGS84', units='m')
+DST_PROJ = pyproj.Proj(
+    proj='moll',
+    lon_0=-70,
+    x_0=0,
+    y_0=0,
+    ellps='WGS84',
+    datum='WGS84',
+    units='m',
+)
+
 
 def project_point(pt):
-  x, y = pyproj.transform(SRC_PROJ, DST_PROJ, pt[0], pt[1])
-  return (x / 2 + MAGIC_MOLL_NUMBER) / (MAGIC_MOLL_NUMBER * 2), (MAGIC_MOLL_NUMBER - y) / (MAGIC_MOLL_NUMBER * 2)
+    x, y = pyproj.transform(SRC_PROJ, DST_PROJ, pt[0], pt[1])
+    return (x / 2 + MAGIC_MOLL_NUMBER) / (MAGIC_MOLL_NUMBER * 2), (
+        MAGIC_MOLL_NUMBER - y
+    ) / (MAGIC_MOLL_NUMBER * 2)
 
 
 def extract_shapes(shape):
-  points = [project_point(pt) for pt in shape.points]
-  if shape.parts == [0]:
-    res = [list(points)]
-  else:
-    parts_idx = list(shape.parts) + [len(points)]
-    res = [points[parts_idx[i]: parts_idx[i + 1]] for i in range(len(shape.parts))]
-  return res
+    points = [project_point(pt) for pt in shape.points]
+    if shape.parts == [0]:
+        res = [list(points)]
+    else:
+        parts_idx = list(shape.parts) + [len(points)]
+        res = [
+            points[parts_idx[i] : parts_idx[i + 1]]
+            for i in range(len(shape.parts))
+        ]
+    return res
 
 
 def index_shape(sh, all_points, all_points_index):
-  res = []
-  sum_x = 0
-  sum_y = 0
-  prev_key = None
-  for pt in sh:
-    sum_x += pt[0]
-    sum_y += pt[1]
-    key = '%2.5f-%2.5f' % pt
-    if key == prev_key:
-      continue
-    prev_key = key
-    if not key in all_points_index:
-      all_points_index[key] = len(all_points)
-      all_points.append(pt)
-    res.append(all_points_index[key])
-  # if the first and last are the same, eliminate, the system will close the loop
-  if res[0] == res[-1]:
-    res = res[:-1]
-  return {'x': sum_x / len(sh),
-          'y': sum_y / len(sh),
-          'area': calc_area(sh),
-          'bridges': [],
-          'pts': res}
+    res = []
+    sum_x = 0
+    sum_y = 0
+    prev_key = None
+    for pt in sh:
+        sum_x += pt[0]
+        sum_y += pt[1]
+        key = '%2.5f-%2.5f' % pt
+        if key == prev_key:
+            continue
+        prev_key = key
+        if not key in all_points_index:
+            all_points_index[key] = len(all_points)
+            all_points.append(pt)
+        res.append(all_points_index[key])
+    # if the first and last are the same, eliminate, the system will close the loop
+    if res[0] == res[-1]:
+        res = res[:-1]
+    return {
+        'x': sum_x / len(sh),
+        'y': sum_y / len(sh),
+        'area': calc_area(sh),
+        'bridges': [],
+        'pts': res,
+    }
+
 
 def distance_squared(pt1, pt2):
-  dx = pt1[0] - pt2[0]
-  dy = pt1[1] - pt2[1]
-  return dx * dx * 4 + dy * dy
+    dx = pt1[0] - pt2[0]
+    dy = pt1[1] - pt2[1]
+    return dx * dx * 4 + dy * dy
+
 
 def main():
-  r = shapefile.Reader('data/cb_2015_us_state_20m')
-  labels = [x[0] for x in r.fields]
-  all_points = []
-  all_points_index = {}
-  indexed_shapes = [[index_shape(sh, all_points, all_points_index) for sh in extract_shapes(shape)]
-                    for shape in r.shapes()]
-  sizes = [sum(sh['area'] for sh in shapes) for shapes in indexed_shapes]
+    r = shapefile.Reader('data/cb_2015_us_state_20m')
+    labels = [x[0] for x in r.fields]
+    all_points = []
+    all_points_index = {}
+    indexed_shapes = [
+        [
+            index_shape(sh, all_points, all_points_index)
+            for sh in extract_shapes(shape)
+        ]
+        for shape in r.shapes()
+    ]
+    sizes = [sum(sh['area'] for sh in shapes) for shapes in indexed_shapes]
 
-  counts = Counter()
-  for shapes in indexed_shapes:
-    for shape in shapes:
-      for pt in shape['pts']:
-        counts[pt] += 1
-  dupes = {k for k, v in counts.items() if v > 1}
-  for shapes in indexed_shapes:
-    for shape in shapes:
-      shape['island'] = not any(pt in dupes for pt in shape['pts'])
+    counts = Counter()
+    for shapes in indexed_shapes:
+        for shape in shapes:
+            for pt in shape['pts']:
+                counts[pt] += 1
+    dupes = {k for k, v in counts.items() if v > 1}
+    for shapes in indexed_shapes:
+        for shape in shapes:
+            shape['island'] = not any(pt in dupes for pt in shape['pts'])
 
-  total_size = sum(sizes)
-  recs = [dict(zip(labels, x)) for x in r.records()]
+    total_size = sum(sizes)
+    recs = [dict(zip(labels, x)) for x in r.records()]
 
-  point_to_shape = {}
-  for idx, shapes in enumerate(indexed_shapes):
-    geoid = recs[idx]['GEOID']
-    for shape in shapes:
-      for pt in shape['pts']:
-        point_to_shape[pt] = (geoid, shape)
+    point_to_shape = {}
+    for idx, shapes in enumerate(indexed_shapes):
+        geoid = recs[idx]['GEOID']
+        for shape in shapes:
+            for pt in shape['pts']:
+                point_to_shape[pt] = (geoid, shape)
 
-  # Find for islands nearby anchors.
-  # This doesn't make too much sense since we're in a non distance preserving
-  # projection. We should probably *also* save the lat/lng for each shape
-  # and then use the spherical distance here. Oh well.
-  geo_index = defaultdict(list)
-  for idx, pt in enumerate(all_points):
-    geo_index[int(500 * pt[1]) * 500 + int(500 * pt[0])].append((idx, pt, point_to_shape[idx]))
+    # Find for islands nearby anchors.
+    # This doesn't make too much sense since we're in a non distance preserving
+    # projection. We should probably *also* save the lat/lng for each shape
+    # and then use the spherical distance here. Oh well.
+    geo_index = defaultdict(list)
+    for idx, pt in enumerate(all_points):
+        geo_index[int(500 * pt[1]) * 500 + int(500 * pt[0])].append(
+            (idx, pt, point_to_shape[idx])
+        )
 
-  for idx, shapes in enumerate(indexed_shapes):
-    geoid = recs[idx]['GEOID']
-    for shape in shapes:
-      if shape['island']:
-        min_x = min(all_points[p][0] for p in shape['pts'])
-        max_x = max(all_points[p][0] for p in shape['pts'])
-        min_y = min(all_points[p][1] for p in shape['pts'])
-        max_y = max(all_points[p][1] for p in shape['pts'])
-        min_x = max(0, int((min_x - 4 / 500) * 500))
-        max_x = min(500, int((max_x + 4 / 500) * 500))
-        min_y = max(0, int((min_y - 6 / 500) * 500))
-        max_y = min(500, int((max_y + 6 / 500) * 500))
-        own_pts = set(shape['pts'])
+    for idx, shapes in enumerate(indexed_shapes):
+        geoid = recs[idx]['GEOID']
+        for shape in shapes:
+            if shape['island']:
+                min_x = min(all_points[p][0] for p in shape['pts'])
+                max_x = max(all_points[p][0] for p in shape['pts'])
+                min_y = min(all_points[p][1] for p in shape['pts'])
+                max_y = max(all_points[p][1] for p in shape['pts'])
+                min_x = max(0, int((min_x - 4 / 500) * 500))
+                max_x = min(500, int((max_x + 4 / 500) * 500))
+                min_y = max(0, int((min_y - 6 / 500) * 500))
+                max_y = min(500, int((max_y + 6 / 500) * 500))
+                own_pts = set(shape['pts'])
 
-        own_cnt = 0
-        near = []
-        for x in range(min_x, max_x):
-          for y in range(min_y, max_y):
-            for pt in geo_index[x + y * 500]:
-              if not pt[0] in own_pts:
-                near.append(pt)
-              else:
-                own_cnt += 1
+                own_cnt = 0
+                near = []
+                for x in range(min_x, max_x):
+                    for y in range(min_y, max_y):
+                        for pt in geo_index[x + y * 500]:
+                            if not pt[0] in own_pts:
+                                near.append(pt)
+                            else:
+                                own_cnt += 1
 
-        shape_center = (shape['x'], shape['y'])
-        bridges = []
-        for idx, pt, (pt_cc, other_shape) in sorted(near, key=lambda other:distance_squared(other[1], shape_center)):
-          if other_shape['island'] and other_shape['area'] < shape['area']:
-            # only bridge to larger land masses to avoid conflicts
-            continue
-          distance = math.sqrt(distance_squared(pt, shape_center))
-          for _, bridge_pt, _ in bridges:
-            if math.sqrt(distance_squared(bridge_pt, pt)) < distance:
-              break
-          else:
-            bridges.append((idx, pt, pt_cc))
-        shape['bridges'] = [(idx, pt[0] - shape['x'], pt[1] - shape['y'], pt_cc) for idx, pt, pt_cc in bridges]
+                shape_center = (shape['x'], shape['y'])
+                bridges = []
+                for idx, pt, (pt_cc, other_shape) in sorted(
+                    near,
+                    key=lambda other: distance_squared(other[1], shape_center),
+                ):
+                    if (
+                        other_shape['island']
+                        and other_shape['area'] < shape['area']
+                    ):
+                        # only bridge to larger land masses to avoid conflicts
+                        continue
+                    distance = math.sqrt(distance_squared(pt, shape_center))
+                    for _, bridge_pt, _ in bridges:
+                        if (
+                            math.sqrt(distance_squared(bridge_pt, pt))
+                            < distance
+                        ):
+                            break
+                    else:
+                        bridges.append((idx, pt, pt_cc))
+                shape['bridges'] = [
+                    (idx, pt[0] - shape['x'], pt[1] - shape['y'], pt_cc)
+                    for idx, pt, pt_cc in bridges
+                ]
 
-  countries = [{'name': str(rec['STUSPS']),
-                'cc': rec['GEOID'],
-                'shapes': shapes,
-                'weight': size / total_size}
-               for rec, shapes, size in zip(recs, indexed_shapes, sizes)
-               if rec['GEOID'] and not rec['GEOID'] == 'PR']
+    countries = [
+        {
+            'name': str(rec['STUSPS']),
+            'cc': rec['GEOID'],
+            'shapes': shapes,
+            'weight': size / total_size,
+        }
+        for rec, shapes, size in zip(recs, indexed_shapes, sizes)
+        if rec['GEOID'] and not rec['GEOID'] == 'PR'
+    ]
 
-  states_by_code = {country['cc']: country for country in countries}
-  for cc1, cc2 in EXTRA_BRIDGES:
-    country1 = states_by_code[cc1]
-    country2 = states_by_code[cc2]
-    shape1 = max(country1['shapes'], key=lambda shape:shape['area'])
-    shape2 = max(country2['shapes'], key=lambda shape:shape['area'])
-    if shape1['area'] < shape2['area']:
-      shape1, shape2 = shape2, shape1
-      country1, country2 = country2, country1
+    states_by_code = {country['cc']: country for country in countries}
+    for cc1, cc2 in EXTRA_BRIDGES:
+        country1 = states_by_code[cc1]
+        country2 = states_by_code[cc2]
+        shape1 = max(country1['shapes'], key=lambda shape: shape['area'])
+        shape2 = max(country2['shapes'], key=lambda shape: shape['area'])
+        if shape1['area'] < shape2['area']:
+            shape1, shape2 = shape2, shape1
+            country1, country2 = country2, country1
 
-    shape1_ct = (shape1['x'], shape1['y'])
-    pt_idx = min(shape2['pts'], key=lambda pti:distance_squared(shape1_ct, all_points[pti]))
-    x, y = all_points[pt_idx]
-    shape1['bridges'].append((pt_idx, x - shape1['x'], y - shape1['y'], country1['cc']))
+        shape1_ct = (shape1['x'], shape1['y'])
+        pt_idx = min(
+            shape2['pts'],
+            key=lambda pti: distance_squared(shape1_ct, all_points[pti]),
+        )
+        x, y = all_points[pt_idx]
+        shape1['bridges'].append(
+            (pt_idx, x - shape1['x'], y - shape1['y'], country1['cc'])
+        )
 
+    min_x = min(x for x, y in all_points)
+    min_y = min(y for x, y in all_points)
+    max_x = max(x for x, y in all_points)
+    max_y = max(y for x, y in all_points)
 
-  min_x = min(x for x, y in all_points)
-  min_y = min(y for x, y in all_points)
-  max_x = max(x for x, y in all_points)
-  max_y = max(y for x, y in all_points)
+    all_points = [
+        (
+            0.1 + 0.8 * (x - min_x) / (max_x - min_x),
+            0.1 + 0.8 * (y - min_y) / (max_y - min_y),
+        )
+        for x, y in all_points
+    ]
 
-  all_points = [(0.1 + 0.8 * (x - min_x) / (max_x - min_x), 0.1 + 0.8 * (y - min_y) / (max_y - min_y)) for x, y in all_points]
+    with open('data/states.js', 'w') as fout:
+        fout.write(
+            'STATES='
+            + json.dumps(countries, indent=2)
+            + ';\n'
+            + 'POINT_INDEX='
+            + json.dumps(all_points, indent=2)
+            + ';\n'
+        )
 
-  with open('data/states.js', 'w') as fout:
-    fout.write('STATES=' + json.dumps(countries, indent=2) + ';\n' +
-               'POINT_INDEX=' + json.dumps(all_points, indent=2) + ';\n'
-    )
 
 if __name__ == '__main__':
-  main()
-
-
-
-
-
+    main()
